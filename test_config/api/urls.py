afb29a090d1f3ef29d89420f8a03e4518704c7eb
isatra21 <isadustintran@gmail.com>
2025-11-26 20:44:29 +0700

    update django rag

--- test_config/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('bot', views.getData),
-    path('files', views.upload_files),
-]
--- test_config/pdf_rag/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-
-    path('index/', views.index_view, name='index_view'),
-    path('demo/', views.demo, name='demo')
-]
